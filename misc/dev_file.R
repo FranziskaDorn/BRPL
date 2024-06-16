4934fa218eef49e2a8c078c6798cc43a090d181d
René-M Kruse <rene-marcel.kruse@uni-goettingen.de>
2024-06-16 17:14:17 +0200

    Aufräumen.

--- misc/dev_file.R
+++ misc/dev_file.R
@@ -4,17 +4,14 @@ gc(reset = TRUE)
 # Set-Up
 set.seed(5000)
 
-load("data/total.Rda")
-load("data/total1.Rda")
 
 # Load all
 devtools::load_all()
 
+data("nutritionpoverty")
+
 # Anwendung
 # myecdf(data = total1, var = "INval")
-
-
-bivqfun(data = total1, var1="NUval", var2="INval", tau = 0.25)
-
-data("nutritionpoverty")
 bivqfun(data = nutritionpoverty, var1="NUval", var2="INval", tau = 0.25)
+
+
