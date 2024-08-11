bf7207a0e9b13a388a6dc3a217d0e7215aa059fc
René-M Kruse <rene-marcel.kruse@uni-goettingen.de>
2024-08-11 12:02:40 +0200

    Neues Datenset inkl. Beschreibung von Franzi.

--- R/data.R
+++ R/data.R
@@ -1,3 +1,14 @@
+#' Poverty Line Data
+#'
+#' @format ## `pov_line_data_wt`
+#' A data frame with 30,221,284 rows and 4 columns:
+#' \describe{
+#'   \item{leisure}{Weekly minutes per capita spent on leisure time. Defined as in Dorn et al. 2023.}
+#'   \item{inc_expenses}{Monthly income per capita. Defined as in Dorn et al. 2023.}
+#' }
+#' @source <https://www.inegi.org.mx/programas/enigh/nc/2018/>
+'pov_line_data_wt'
+
 #' Example dataset nutritionpoverty
 #'
 #' A subset of data from Franzi.
