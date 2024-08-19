ab2263b065cf6397961e843226e62f4f88c999bd
René-M Kruse <rene-marcel.kruse@uni-goettingen.de>
2024-08-19 18:16:58 +0200

    Vergessen etwas auszukommentieren.

--- R/main.R
+++ R/main.R
@@ -1,17 +1,4 @@
-#' Main File for the wrapper
-#'
-#' @importFrom rlang is_character
-#' @importFrom stats approxfun quantile na.omit
-#' @importFrom graphics abline lines points
-#' @param data data source of interest. Has to be a data.frame.
-#' @param var1 Variable of interest. Has to be a character.
-#' @param var2 Variable of interest. Has to be a character.
-#' @param tau Minimal possible value for given variable.
-#' @param nalpha Minimal possible value for given variable.
-#' @param plot Logical. When `TRUE` a graphic of the results will be produced.
-#'
-#' @export
-bivqfun <- function(data, var1, var2, tau = 0.5, nalpha = 100, plot = TRUE) {
+bivqfun <- function(data, var1, var2, tau = 0.5, nalpha = 100) {
 
   # Input checks:
   stopifnot(
@@ -51,46 +38,69 @@ bivqfun <- function(data, var1, var2, tau = 0.5, nalpha = 100, plot = TRUE) {
   faz <- c(min(bivhhi[, 1]), max(data[[var2]], na.rm = TRUE))
   faa <- c(max(data[[var1]], na.rm = TRUE) + 1, min(bivhhi[, 2]))
   plvar2 <- rbind(faz, bivhhi, faa)
-  print(plvar2[, 1])
+
   # Calculate abovevar2 indicator
   data$abovevar2 <- rep(0, nrow(data))
   for (i in 1:nrow(data)) {
-  x <- c(data[[var1]][i], data[[var2]][i])
-  if (all(x[1] > plvar2[, 1])) {
-    data$abovevar2[i] <- 0
-  } else {
-    ind <- min(which(x[1] <= plvar2[, 1]))
-    if (x[2] > plvar2[ind, 2]) {
-    data$abovevar2[i] <- 1
+    x <- c(data[[var1]][i], data[[var2]][i])
+    if (all(x[1] > plvar2[, 1])) {
+      data$abovevar2[i] <- 0
+    } else {
+      ind <- min(which(x[1] <= plvar2[, 1]))
+      if (x[2] > plvar2[ind, 2]) {
+        data$abovevar2[i] <- 1
+      }
     }
   }
-  }
 
-  if (plot) {
-    dbvar2 <- subset(data, abovevar2 == 0)
-    View(dbvar2)
-    davar2 <- subset(data, abovevar2 == 1)
-    View(davar2)
-
-    plot(
-      plvar2[, 1], plvar2[, 2], type = "l", col = "black", lwd = 3,
-      xlab = "Variable 1", ylab = "Variable 2",
-      xlim = c(min(dbvar2[[var1]]) * 0.9, faa[1] * 1.1),
-      ylim = c(0, faz[2]),
-      main = paste("Bivariate relative discriminator with given tau of", tau)
-    )
-
-    points(dbvar2[[var1]], dbvar2[[var2]], col = "blue")
-    points(davar2[[var1]], davar2[[var2]], col = "darkgreen")
-
-    abline(v = faz[[1]], col = "black", lwd = 3)
-    abline(h = faa[[2]], col = "black", lwd = 3)
-    lines(plvar2[, 1], plvar2[, 2], col = "black", lwd = 3)
-  }
-
-  print("Funktioniert.")
+  # Return results as an object of class 'bivq'
+  output <- list(
+    data = data,
+    tau = tau,
+    plvar2 = plvar2,
+    faz = faz,
+    faa = faa,
+    var1 = var1,
+    var2 = var2
+  )
+  class(output) <- "bivq"
+  return(output)
 }
 
-# print.bivqfun <- function(obj){
+
+plot.bivq <- function(x, ...) {
+  data <- x$data
+  tau <- x$tau
+  plvar2 <- x$plvar2
+  faz <- x$faz
+  faa <- x$faa
+  var1 <- x$var1
+  var2 <- x$var2
+
+  dbvar2 <- subset(data, abovevar2 == 0)
+  davar2 <- subset(data, abovevar2 == 1)
+
+  plot(
+    plvar2[, 1], plvar2[, 2], type = "l", col = "black", lwd = 3,
+    xlab = "Variable 1", ylab = "Variable 2",
+    xlim = c(min(dbvar2[[var1]]) * 0.9, faa[1] * 1.1),
+    ylim = c(0, faz[2]),
+    main = paste("Bivariate relative discriminator with given tau of", tau)
+  )
+
+  points(dbvar2[[var1]], dbvar2[[var2]], col = "blue")
+  points(davar2[[var1]], davar2[[var2]], col = "darkgreen")
+
+  abline(v = faz[[1]], col = "black", lwd = 3)
+  abline(h = faa[[2]], col = "black", lwd = 3)
+  lines(plvar2[, 1], plvar2[, 2], col = "black", lwd = 3)
+}
+
 #
-# }
+# data("nutritionpoverty")
+#
+# # Calculate values and store them in an object
+# result <- bivqfun(nutritionpoverty, "NUval", "INval", tau = 0.5, nalpha = 100)
+#
+# # Plot the result
+# plot(result)
