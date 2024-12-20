eb07be43808781e00c3bba5861a9c5b277cb1db6
René-M Kruse <rene-marcel.kruse@uni-goettingen.de>
2024-12-20 21:09:53 +0100

    Aufräumen.

--- misc/testing_env.R
+++
@@ -1,6 +0,0 @@
-library(devtools)
-
-data("nutritionpoverty")
-bivqfun_old(data = nutritionpoverty, var1="NUval", var2="INval", tau = 0.25)
-
-bivqfun(data = nutritionpoverty, var1="NUval", var2="INval", tau = 0.25)
